11a0a1398574f461634b43684c211ddcefe0041a
Mkainouch <kainouch.med@gmail.com>
2020-03-18 10:59:00 +0100

    first commit

---
+++ js/inscription.js
@@ -0,0 +1,16 @@
+var user = document.getElementById('userName'),
+    loginPass = document.getElementById('loginPass'),
+    cin = document.getElementById('cin'),
+    firstName = document.getElementById('firstName'),
+    lastName = document.getElementById('lastName'),
+    birthday = document.getElementById('birthday'),
+    email = documen.getElementById('email'),
+    confEmail = document.getElementById('confEmail'),
+    registrationPass = document.getElementById('registrationPass'),
+    confPassword = document.getElementById('confPassword'),
+    phonNum = document.getElementById('phonNum')
+function myfocus(){
+    var inputs = [user, loginPass, cin, firstName, lastName, birthday, 
+        email, confEmail, registrationPass, confPassword, phonNum];
+    
+}
